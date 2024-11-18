380234b500696715964414a93b6c8b823c66e1b6
trueberryless <trueberryless+github@gmail.com>
2024-11-18 09:22:54 +0100

    rework package.json

--- packages/generator-starlight-plugin/__tests__/app.ts
+++ packages/generator-starlight-plugin/__tests__/app.ts
@@ -1,14 +1,14 @@
 "use strict";
-const path = require("path");
-const assert = require("yeoman-assert");
-const helpers = require("yeoman-test");
+import path from "path";
+import assert from "yeoman-assert";
+import helpers from "yeoman-test";
 
 describe("@trueberryless-org/generator-starlight-plugin:app", () => {
   beforeAll(() => {
     return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
       repositoryName: "generator-starlight-plugin",
       githubOwner: "trueberryless-org",
-      dockerOwner: "trueberryless",
+      dockerOwner: "trueberryless"
     });
   });
 
@@ -17,7 +17,7 @@ describe("@trueberryless-org/generator-starlight-plugin:app", () => {
     assert.file(["package.json"]);
     assert.file([
       ".github/workflows/welcome-bot.yaml",
-      ".github/workflows/release.yaml",
+      ".github/workflows/release.yaml"
     ]);
 
     assert.fileContent();
